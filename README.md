3eb947b26e249ca856e0444b304d5146976fd411
Hirota Taketoshi <t_hirota0407@yahoo.co.jp>
2021-08-26 23:57:40 +0900

    add mlflow samples

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # mlflow-quick-sample
+
+[[_TOC_]]
+
+## Overview
+
+MLflow(Tracking)を動作させるサンプル。ローカルで動作させることを想定。
+
+## Install
+
+```
+python -m pip install -r requirements.txt
+```
+
+## Quick Sample
+
+以下を実行してhttp://localhost:5000にアクセスする。
+
+```
+cd sample
+python mlflow_sample.py
+mlflow ui --host 0.0.0.0 --port 5000
+```
+
+## Reference
+
+* [MLflow Tracking](https://www.mlflow.org/docs/latest/tracking.html)
+* [MLflowで実験管理入門](https://future-architect.github.io/articles/20200626/)
---
+++ sample/mlflow_sample.py
@@ -0,0 +1,37 @@
+import mlflow
+
+
+def main():
+
+    # set experiment
+    experiment_name: str = "test experiment"
+    try:
+        print(f"create experiment: {experiment_name}")
+        mlflow.create_experiment(experiment_name)
+    except mlflow.exceptions.MlflowException:
+        print(f"experiment '{experiment_name}' already exists")
+    mlflow.set_experiment(experiment_name)
+
+    # start run (create run id)
+    with mlflow.start_run():
+
+        # log param
+        mlflow.log_param("param_int", 5)
+        mlflow.log_param("param_str", "hoge")
+        mlflow.log_param("param_list", [1, 2, 3, 4, 5])
+        mlflow.log_param("param_dict", {"key": "value"})
+        print(f"logged parameters")
+
+        # log metric
+        for i in range(10):
+            mlflow.log_metric("accuracy", i / 10, step=i+1)
+        print(f"logged metrics")
+
+        # log artifact
+        mlflow.log_artifact("./artifact.txt")
+        print(f"logged artifacts")
+
+    print(f"finish!!")
+
+if __name__ == "__main__":
+    main()
